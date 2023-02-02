7cded411d9a67a0a68f79d71d86352cfa07a9877
Tarun9290 <111385923+Tarun9290@users.noreply.github.com>
2023-02-03 01:39:47 +0530

    Add files via upload

---
+++ personal_financemanagement.py
@@ -0,0 +1,12 @@
+#Personal Finance Manager:
+#A desktop application that helps users manage their personal finances.
+
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Personal Finance Manager")
+
+label = tk.Label(root, text="Welcome to the Personal Finance Manager!")
+label.pack()
+
+root.mainloop()
